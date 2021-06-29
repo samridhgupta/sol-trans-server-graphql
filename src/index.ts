446862e8454546018439d6698a6d8db034be527f
Samridh Gupta <samridhgupta@gmail.com>
2021-06-29 12:13:37 +0530

    Basic Express API working with a Hello world endpoint

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express';
+import express from 'express';
+
+
+const app = express();
+const port = process.env.PORT || '8000';
+
+
+interface HelloResponse {
+    hello: string;
+}
+
+type HelloBuilder = (name: string) => HelloResponse;
+
+const helloBuilder: HelloBuilder = name => ({ hello: name });
+
+export const rootHandler = (_req: Request, res: Response) => {
+    return res.send('API is working 🤓');
+};
+
+export const helloHandler = (req: Request, res: Response) => {
+    const { params } = req;
+    const { name = 'World' } = params;
+    const response = helloBuilder(name);
+
+    return res.json(response);
+};
+
+
+app.get('/', rootHandler);
+app.get('/hello/:name', helloHandler);
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
